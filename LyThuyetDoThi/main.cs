9726d536e49c046d6a3cac3e4bbe80de4b4bffed
Thanh Nguyen <thanh.nguyen@beetech.com.vn>
2023-05-25 21:51:37 +0700

    Them dinh, them canh

--- LyThuyetDoThi/main.cs
+++ LyThuyetDoThi/main.cs
@@ -63,9 +63,16 @@ namespace LyThuyetDoThi
                 int x = dinh.ToadoX;
                 int y = dinh.ToadoY;
                 g.FillEllipse(Brushes.Red, x, y, 20, 20);
-                g.DrawString(dinh.Ten, new Font("Arial", 14), Brushes.Black, x + 1, y - 20);
+                g.DrawString(dinh.Ten, new Font("Arial", 14), Brushes.Black, x - 7, y - 20);
             }
         }
+
+        private bool KiemTraTonTaiDinh(string tenDinh)
+        {
+            // Phương thức Exists dùng để duyệt phần tử trong danh sách Dinh, nếu phần tử tồn tại trong danh sách thì trả về true, ngược lại trả về false
+            return dt.Dinh.Exists(dinh => dinh.Ten == tenDinh);
+        }
+
 
         private void bt_themDinh_Click(object sender, EventArgs e)
         {
@@ -76,7 +83,13 @@ namespace LyThuyetDoThi
             {
                 // Kiểm tra đầu vào
                 MessageBox.Show("Vui lòng nhập đỉnh", "Warming", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
+            if (KiemTraTonTaiDinh(tenDinh))
+            {
+                MessageBox.Show("Đỉnh đã tồn tại", "Warming", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txbThemDinh.Text = "";
+            }    
             else
             {
                 // Tạo đỉnh đầu tiên
@@ -127,13 +140,81 @@ namespace LyThuyetDoThi
             }
         }
 
+        private void veCanh()
+        {
+            // Lấy graphics từ panel_Graph
+            Graphics g = panel_Graph.CreateGraphics();
+
+            // Vẽ các cạnh trên đồ thị
+            foreach (Canh canh in dt.Canh)
+            {
+                // Vẽ cạnh dưới dạng đường thẳng
+                int x1 = canh.Dinhdau.ToadoX;
+                int y1 = canh.Dinhdau.ToadoY;
+                int x2 = canh.Dinhcuoi.ToadoX;
+                int y2 = canh.Dinhcuoi.ToadoY;
+                string trongso = Convert.ToString(canh.Trongso);
+                Pen p = new Pen(Color.Black, 5);
+                g.DrawLine(p, x1 + 10 , y1 + 10, x2 + 10, y2 + 10);     // các tọa độ + (đường kính hình tròn của đỉnh được vẽ / 2)
+
+                // Vẽ trọng số
+                Font font = new Font("Arial", 14);
+                Brush brush = Brushes.Black;
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.LineAlignment = StringAlignment.Center;    // Căn chỉnh dòng ở giữa
+                stringFormat.Alignment = StringAlignment.Center;        // Căn lề giữa
+                int trungdiemX = (x1 + x2) / 2;                         // Tọa độ x tại vị trí trung tâm của cạnh
+                int trungdiemY = (y1 + y2) / 2;                         // Tọa độ y tại vị trí trung tâm của cạnh
+                g.DrawString(trongso, font, brush, trungdiemX, trungdiemY, stringFormat);
+            }
+        }
+
         private void bt_themCanh_Click(object sender, EventArgs e)
         {
             // Kiểm tra đầu vào
             if (txbDinhDau.Text == "" || txbDinhCuoi.Text == "")
             {
-                MessageBox.Show("Không thể thêm cạnh", "Warming", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }    
+                MessageBox.Show("Vui lòng nhập đỉnh", "Warming", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string DinhDau = txbDinhDau.Text;
+                string DinhCuoi = txbDinhCuoi.Text;
+                int TrongSo = Convert.ToInt16(txbTrongSo.Text);
+                
+                /* Phương thức FirstOrDefault trả về phần tử đầu tiên của danh sách Dinh trong class DoThi theo điều kiện, nếu không có phần tử thoả mãn,
+                   kết quả trả về sẽ là giá trị default của object (thường là null) */
+                Dinh DinhDauDaTonTai = dt.Dinh.FirstOrDefault(d => d.Ten == DinhDau);
+                Dinh DinhCuoiDaTonTai = dt.Dinh.FirstOrDefault(d => d.Ten == DinhCuoi);
+                if (DinhDauDaTonTai == null || DinhCuoiDaTonTai == null)
+                {
+                    MessageBox.Show("Không tìm thấy đỉnh", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else
+                {
+                    Canh newCanh = new Canh();
+                    newCanh.Dinhdau = DinhDauDaTonTai;
+                    newCanh.Dinhcuoi = DinhCuoiDaTonTai;
+                    newCanh.Trongso = TrongSo;
+                    dt.Canh.Add(newCanh);
+                    veCanh();
+                }
+                // Clear giá trị đầu vào
+                txbDinhDau.Text = "";
+                txbDinhCuoi.Text = "";
+                txbTrongSo.Text = "";
+            }     
+        }
+        private void tomau_Canh(Graphics g, int x1, int y1, int x2, int y2, Color color1, Color color2)
+        {
+
+            // Tạo đối tượng Brush với màu sắc
+            LinearGradientBrush brush = new LinearGradientBrush(new Point(x1, y1), new Point(x2, y2), color1, color2);
+
+            // Vẽ cạnh với màu gradient
+            Pen p1 = new Pen(brush, 5);
+            g.DrawLine(p1, x1 + 10, y1 + 10, x2 + 10, y2 + 10);
         }
     }
 }
